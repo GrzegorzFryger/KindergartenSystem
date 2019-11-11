ab35b5ce1fb8650d3c95c132cc4408ee421f3bf6
GrzegorzFryger <s12583@pjwstk.edu.pl>
2019-11-11 19:28:16 +0100

    Added Core module to project
    Took 11 minutes

---
+++ Core/src/main/java/pl/edu/pja/core/KindergartenSystem.java
@@ -0,0 +1,11 @@
+package pl.edu.pja.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenSystem {
+	public static void main(String[] args) {
+		SpringApplication.run(KindergartenSystem.class, args);
+	}
+}
