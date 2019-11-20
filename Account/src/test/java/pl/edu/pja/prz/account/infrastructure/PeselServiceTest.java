dbcd732f21c5865d5d172bfe210b9cdc0461532e
Grzegorz <s12583@pjwstk.edu.pl>
2019-11-20 13:11:46 +0100

    Add tests to peselService

--- Account/src/test/java/pl/edu/pja/prz/account/infrastructure/PeselServiceTest.java
+++ Account/src/test/java/pl/edu/pja/prz/account/infrastructure/PeselServiceTest.java
@@ -20,11 +20,16 @@ class PeselServiceTest {
 	public void Should_ConvertPasselNumberToDate_When_NumberOfPeselIsCorrect() {
 		//given
 		var peselNumberBetween1900_1999 = "70020556117";
-
+		var peselNumberBetween2000_2099 = "06320538371";
+		var peselNumberBetween2100_2199 = "00440758725";
 		//when
-		var date = peselService.generateDateFromPesel(peselNumberBetween1900_1999);
+		var date1900 = peselService.generateDateFromPesel(peselNumberBetween1900_1999);
+		var date2000 = peselService.generateDateFromPesel(peselNumberBetween2000_2099);
+		var date2100 = peselService.generateDateFromPesel(peselNumberBetween2100_2199);
 		//then
-		assertEquals(LocalDate.of(1970, 2, 5), date);
+		assertEquals(LocalDate.of(1970, 2, 5), date1900);
+		assertEquals(LocalDate.of(2006, 12, 5), date2000);
+		assertEquals(LocalDate.of(2100, 4, 7), date2100);
 	}
 
 }
