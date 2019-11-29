1ee7ef6cb1cc127063372019f444f1fce72c4348
Adam <s15814@pjwstk.edu.pl>
2019-11-29 21:14:10 +0100

    Initialize Groups Module

---
+++ groups/src/main/java/pl/edu/pja/prz/groups/GroupsModule.java
@@ -0,0 +1,12 @@
+package pl.edu.pja.prz.groups;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupsModule {
+    public static void main(String[] args) {
+        SpringApplication.run(GroupsModule.class, args);
+    }
+}
