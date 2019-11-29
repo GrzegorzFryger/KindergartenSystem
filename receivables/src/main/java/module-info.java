475be3d95d708cdd916995235193481ecb24f6e5
Sebastian Kaminski <skaminskirepo@gmail.com>
2019-11-29 23:59:45 +0100

    Add initial module configuration

--- receivables/src/main/java/module-info.java
+++ receivables/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 open module receivables {
     requires spring.boot.autoconfigure;
     requires spring.boot;
+    requires spring.context;
+    requires org.slf4j;
+    requires java.annotation;
 
     exports pl.edu.pja.prz.receivables;
 }
