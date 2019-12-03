c7fdeef424cb0eda8e6ad980374c7115a8d45ead
Grzegorz <s12583@pjwstk.edu.pl>
2019-12-03 19:17:52 +0100

    Refactor abstract factory

--- account/src/main/java/pl/edu/pja/prz/account/model/AccountAbstractFactory.java
+++ account/src/main/java/pl/edu/pja/prz/account/model/AccountAbstractFactory.java
@@ -1,14 +1,31 @@
 package pl.edu.pja.prz.account.model;
 
-import pl.edu.pja.prz.account.model.enums.EmployeeType;
 import pl.edu.pja.prz.account.model.value.Address;
 import pl.edu.pja.prz.account.model.value.FullName;
 import pl.edu.pja.prz.account.model.value.Password;
 import pl.edu.pja.prz.account.model.value.Phone;
 
-abstract class AccountAbstractFactory {
-	Employee creteEmployee(Address address, FullName fullName,
-	                       Phone phone, Password password, String email, EmployeeType employeeType) {
-		return new Employee(address,fullName,phone,password,email, employeeType);
+abstract class AccountAbstractFactory implements AccountFactory {
+
+	protected abstract Employee createTeacher(Address address, FullName fullName, Phone phone,
+	                                          Password password, String email);
+
+	protected abstract Guardian createGuardian(Address address, FullName fullName, Phone phone,
+	                                           Password password, String email);
+
+	public AccountAbstractFactory() {
 	}
-}
+
+	@Override
+	public <T extends  Account> T createStandardAccount(Address address, FullName fullName,
+	                                                    Phone phone, Password password, String email, Class<T> tClass) {
+		if(tClass.getTypeName().equals(Employee.class.getName())) {
+			return tClass.cast(createTeacher(address, fullName, phone, password, email));
+		}
+		else if(tClass.getTypeName().equals(Guardian.class.getName())) {
+			return tClass.cast(createGuardian(address, fullName, phone, password, email));
+		}
+		else throw new IllegalArgumentException("Not Found " + tClass.getName() + " type ");
+	}
+
+ }
