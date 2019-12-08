5c1b1d2d0581712784ba496cd8bc4e87ee9b573f
Grzegorz <s12583@pjwstk.edu.pl>
2019-12-08 16:58:12 +0100

    Add test for employee service

--- account/src/test/java/pl/edu/pja/prz/account/service/EmployeeServiceTest.java
+++ account/src/test/java/pl/edu/pja/prz/account/service/EmployeeServiceTest.java
@@ -3,13 +3,11 @@ package pl.edu.pja.prz.account.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.edu.pja.prz.account.model.AdministratorAccountFactoryImpl;
 import pl.edu.pja.prz.account.model.Employee;
-import pl.edu.pja.prz.account.model.Role;
+import pl.edu.pja.prz.account.model.Person;
 import pl.edu.pja.prz.account.model.value.Address;
 import pl.edu.pja.prz.account.model.value.FullName;
 import pl.edu.pja.prz.account.model.value.Password;
@@ -17,12 +15,11 @@ import pl.edu.pja.prz.account.model.value.Phone;
 import pl.edu.pja.prz.account.repository.EmployeeRepository;
 import pl.edu.pja.prz.account.utilites.PasswordManager;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -32,29 +29,21 @@ class EmployeeServiceTest {
 
 	@Mock private EmployeeRepository employeeRepository;
 	@Mock PasswordManager passwordManager;
-	@Captor
-	private ArgumentCaptor<List<Object>> captor;
-	@Captor
-	private ArgumentCaptor<List<CharSequence>> captor1;
 	private EmployeeService employeeService;
-
 	private Employee employee;
-	private Address address;
-	private Phone phone;
-	private FullName fullName;
-	private Password password;
-	private String email;
-	private Set<Role> roles;
+	private Person updateData;
 
 	@BeforeEach
 	void setUp() {
 		employeeService = new EmployeeService(passwordManager,employeeRepository);
 
-		address = new Address("70-700","City","Street 256");
-		phone = new Phone("123132123");
-		fullName = new FullName("TestName","TestSurname");
-		password = new Password("newPassword");
-		email = "test@test.com";
+		var address = new Address("70-700","City","Street 256");
+		var phone = new Phone("123132123");
+		var fullName = new FullName("TestName","TestSurname");
+		var password = new Password("newPassword");
+		var email = "test@test.com";
+
+		this.employee =  new AdministratorAccountFactoryImpl().createTeacher(address,fullName,phone,password,email);
 	}
 
 	@Test
@@ -63,9 +52,9 @@ class EmployeeServiceTest {
 	}
 
 	@Test
-	void should_ReturnTru_When_EmailAndPasswordMatch() {
+	void should_ReturnTrue_When_EmailAndPasswordMatch() {
 		//given
-		this.employee =  new AdministratorAccountFactoryImpl().createTeacher(address,fullName,phone,password,email);
+
 
 		//when
 		when(employeeRepository.findByEmail(anyString())).thenReturn(Optional.of( employee));
@@ -76,15 +65,43 @@ class EmployeeServiceTest {
 	}
 
 	@Test
-	void updatePersonalData() {
+	void shouldUpdateOnlyChangedPersonalData() {
+		//given
+		var dataToUpdate = new Person( new Address("80-700","UpdateCity","UpdateStreet 256"),
+				new FullName("UpdateTestName","UpdateTestSurname"),
+				new Phone("888888888")
+		);
+
+		//when
+		when(employeeRepository.findById(any())).thenReturn(Optional.of( employee));
+		when(employeeRepository.save(any())).thenReturn(employee);
+		var updatedEmployee = employeeService.updatePersonalData(new UUID(2,6),dataToUpdate);
+
+		//then
+		assertEquals(dataToUpdate.getAddress(),updatedEmployee.getAddress());
+		assertEquals(dataToUpdate.getFullName(),updatedEmployee.getFullName());
+		assertEquals(dataToUpdate.getPhoneNumber(),updatedEmployee.getPhoneNumber());
 	}
 
 	@Test
-	void updateEmail() {
+	void shouldUpdateEmailAddress() {
+
+		var dataToUpdate = "updatedemail@test.com";
+
+		//when
+		when(employeeRepository.findById(any())).thenReturn(Optional.of(employee));
+		when(employeeRepository.save(any())).thenReturn(employee);
+		var updatedEmployee = employeeService.updateEmail(new UUID(2,6),dataToUpdate);
+
+		//then
+		assertEquals(dataToUpdate,updatedEmployee.getEmail());
+
 	}
 
 	@Test
-	void updatePassword() {
+	void shouldUpdatePassword() {
+
+
 	}
 
 	@Test
