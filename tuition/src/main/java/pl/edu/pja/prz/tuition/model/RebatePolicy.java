2cde26814cdb84de4e5deb39b762f99f2b076194
Grzegorz <s12583@pjwstk.edu.pl>
2019-12-14 20:32:11 +0100

    Add constructors and remove redundant setters

--- tuition/src/main/java/pl/edu/pja/prz/tuition/model/RebatePolicy.java
+++ tuition/src/main/java/pl/edu/pja/prz/tuition/model/RebatePolicy.java
@@ -4,40 +4,30 @@ import pl.edu.pja.prz.tuition.model.enums.TypeRebate;
 
 import java.math.BigDecimal;
 
-public class RebatePolicy {
-	private Long id;
+public class RebatePolicy extends BaseEntity<Long> {
 	private String description;
 	private BigDecimal value;
 	private TypeRebate typeRebate;
 
-	public Long getId() {
-		return id;
+	RebatePolicy() {
+	}
+
+	public RebatePolicy(String description, BigDecimal value, TypeRebate typeRebate) {
+		this.description = description;
+		this.value = value;
+		this.typeRebate = typeRebate;
 	}
 
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	public BigDecimal getValue() {
 		return value;
-	}
-
-	public void setValue(BigDecimal value) {
-		this.value = value;
 	}
 
 	public TypeRebate getTypeRebate() {
 		return typeRebate;
 	}
-
-	public void setTypeRebate(TypeRebate typeRebate) {
-		this.typeRebate = typeRebate;
-	}
-
-
 
 }
