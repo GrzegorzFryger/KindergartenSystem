0740bc753c8065a305ed10c85d6250655951b276
Adam <s15814@pjwstk.edu.pl>
2019-12-15 16:33:46 +0100

    Remove unnecessary dependancies
    From pom and module-info

--- groups/src/main/java/module-info.java
+++ groups/src/main/java/module-info.java
@@ -1,12 +1,8 @@
 open module groups {
-    requires spring.boot.autoconfigure;
-    requires spring.boot;
+    requires java.persistence;
     requires spring.context;
     requires org.slf4j;
     requires java.annotation;
-    requires java.persistence;
-    requires java.sql;
-    requires org.apache.commons.lang3;
 
     exports pl.edu.pja.prz.groups;
 }
