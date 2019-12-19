72f77bd7a350094bbd7e34e3c8e25bf2fb2ff942
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2019-12-19 21:04:30 +0100

    fix build by changing repo method parameter type long -> uuid

--- meal/src/main/java/pl/edu/pja/prz/meal/repository/MealRepository.java
+++ meal/src/main/java/pl/edu/pja/prz/meal/repository/MealRepository.java
@@ -7,10 +7,11 @@ import pl.edu.pja.prz.meal.model.enums.MealStatus;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
-	Optional<Meal> findMealByChildIDAndMealStatus(long id, MealStatus status);
-	Optional<Meal> findMealByIdAndMealStatus(long id, MealStatus status);
+	Optional<Meal> findMealByChildIDAndMealStatus(UUID id, MealStatus status);
+	Optional<Meal> findMealByIdAndMealStatus(Long id, MealStatus status);
 	List<Meal> findAllByMealStatus(MealStatus mealStatus);
 }
