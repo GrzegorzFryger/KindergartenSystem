9cb9328819d2d58411dd4d2f7e8c9fb92fe2fdca
Sebastian Kaminski <skaminskirepo@gmail.com>
2019-12-24 02:11:30 +0100

    Disable CSRF protection
    This is temporarily change - functionality will be restored
    in the future. CSRF protection is disabled for now in order to
    simplify Rest Controllers creation.

--- core/src/main/java/pl/edu/pja/prz/core/configuration/SecurityConfig.java
+++ core/src/main/java/pl/edu/pja/prz/core/configuration/SecurityConfig.java
@@ -10,6 +10,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll()
+                .and()
+                .csrf().disable();
     }
 }
