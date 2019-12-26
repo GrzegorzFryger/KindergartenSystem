83f5bf95264b5834252cc9f62cf0799ac479277b
Adam <s15814@pjwstk.edu.pl>
2019-12-27 00:43:52 +0100

    Implement create method
    First attempt, didn't test it properly yet

--- groups/src/main/java/pl/edu/pja/prz/groups/service/GroupService.java
+++ groups/src/main/java/pl/edu/pja/prz/groups/service/GroupService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface GroupService {
 
-    Group createGroup(Group group);
+    Group createGroup(String groupName, String groupDescription);
 
     Group updateGroup(Group group);
 
--- groups/src/main/java/pl/edu/pja/prz/groups/service/GroupServiceImpl.java
+++ groups/src/main/java/pl/edu/pja/prz/groups/service/GroupServiceImpl.java
@@ -1,17 +1,26 @@
 package pl.edu.pja.prz.groups.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.edu.pja.prz.groups.model.Group;
+import pl.edu.pja.prz.groups.model.GroupBuilder;
+import pl.edu.pja.prz.groups.repository.GroupRepository;
 
 import java.util.List;
 
 @Service
 public class GroupServiceImpl implements GroupService {
 
+    @Autowired
+    GroupRepository groupRepository;
 
     @Override
-    public Group createGroup(Group group) {
-        return null;
+    public Group createGroup(String groupName, String groupDescription) {
+        var group = new GroupBuilder()
+                .withGroupName(groupName)
+                .withGroupDescription(groupDescription)
+                .build();
+        return groupRepository.save(group);
     }
 
     @Override
