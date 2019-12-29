2a11ca323c341760483990dfaeff21faae1eadb7
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2019-12-29 11:37:05 +0100

    Add MealServiceImplTest class

--- meal/src/test/java/pl/edu/pja/prz/meal/service/MealDictionaryServiceTest.java
+++ meal/src/test/java/pl/edu/pja/prz/meal/service/MealDictionaryServiceTest.java
@@ -1,6 +1,5 @@
 package pl.edu.pja.prz.meal.service;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -8,18 +7,23 @@ import pl.edu.pja.prz.meal.model.dto.DictionaryObjectDTO;
 
 import java.util.List;
 
+import static org.junit.Assert.assertNotNull;
+
+
 @RunWith(MockitoJUnitRunner.class)
 public class MealDictionaryServiceTest {
 
     private MealDictionaryService mealDictionaryService = new MealDictionaryService();
 
     @Test
-    public void getAllMealTypes() {
+    public void Should_GetAllMealsType() {
         //given
         List<DictionaryObjectDTO> dictionaryObjectDTOList;
         //when
         dictionaryObjectDTOList = mealDictionaryService.getAllMealTypes();
         //then
-        Assert.assertNotNull(dictionaryObjectDTOList);
+        assertNotNull(dictionaryObjectDTOList);
     }
+
+
 }
--- meal/src/test/java/pl/edu/pja/prz/meal/service/MealServiceImplTest.java
+++ meal/src/test/java/pl/edu/pja/prz/meal/service/MealServiceImplTest.java
@@ -1,7 +1,64 @@
 package pl.edu.pja.prz.meal.service;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.edu.pja.prz.meal.exception.MealActivityStatusException;
+import pl.edu.pja.prz.meal.repository.MealPriceListRepository;
+import pl.edu.pja.prz.meal.repository.MealRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class MealServiceImplTest {
 
+    @Mock
+    private MealRepository mealRepository;
+    @Mock
+    private MealPriceListRepository mealPriceListRepository;
+    private MealServiceImpl mealService ;
 
 
+    @BeforeEach
+    public void setup() {
+        mealService = new MealServiceImpl(mealRepository, new MealPriceListServiceImpl(mealPriceListRepository));
+    }
+
+
+    @Test
+    public void createMeal() throws MealActivityStatusException {
+
+    }
+
+    @Test
+    public void getMealByID() {
+    }
+
+    @Test
+    public void deleteMealByID() {
+    }
+
+    @Test
+    public void updateMeal() {
+    }
+
+    @Test
+    public void markMealAsInactiveOnDemand() {
+    }
+
+    @Test
+    public void getAllMeals() {
+    }
+
+    @Test
+    public void getAllActiveMeals() {
+    }
+
+    @Test
+    public void markMealsAsInactiveIfNeeded() {
+    }
+
+    @Test
+    public void isMealPresentByID() {
+    }
 }
