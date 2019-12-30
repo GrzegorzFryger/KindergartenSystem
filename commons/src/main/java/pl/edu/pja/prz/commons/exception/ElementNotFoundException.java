165fcc40d0e0e1d0d4c58ec31a856510c87b64f2
Sebastian Kaminski <skaminskirepo@gmail.com>
2019-12-31 00:55:01 +0100

    Add new exception type - ElementNotFoundException
    This approach should unify error handling within application as whole

---
+++ commons/src/main/java/pl/edu/pja/prz/commons/exception/ElementNotFoundException.java
@@ -0,0 +1,22 @@
+package pl.edu.pja.prz.commons.exception;
+
+public class ElementNotFoundException extends RuntimeException {
+    private final String message;
+
+    public ElementNotFoundException(String element, Long id) {
+        message = buildErrorMessage(element, String.valueOf(id));
+    }
+
+    public ElementNotFoundException(Long id) {
+        message = buildErrorMessage("Element", String.valueOf(id));
+    }
+
+    private String buildErrorMessage(String element, String id) {
+        return "[ERROR] " + element + " with id: " + id + " not found.";
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
