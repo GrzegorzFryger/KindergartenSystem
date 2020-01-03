5547da22cf6f307eadbfbddc6daf4bad162b69d2
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-01-03 17:03:56 +0100

    Add missing @Repository annotation

--- receivables/src/main/java/pl/edu/pja/prz/receivables/repository/TransactionRepository.java
+++ receivables/src/main/java/pl/edu/pja/prz/receivables/repository/TransactionRepository.java
@@ -1,11 +1,13 @@
 package pl.edu.pja.prz.receivables.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.edu.pja.prz.receivables.model.Transaction;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findAllByChildId(UUID childId);
     List<Transaction> findAllByGuardianId(UUID guardianId);
