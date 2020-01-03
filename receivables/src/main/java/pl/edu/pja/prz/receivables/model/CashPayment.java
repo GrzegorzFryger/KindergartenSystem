3c5f31c1018e904500b95e625b0be10e2daea91b
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-01-03 17:20:32 +0100

    Add CashPayment model

---
+++ receivables/src/main/java/pl/edu/pja/prz/receivables/model/CashPayment.java
@@ -0,0 +1,92 @@
+package pl.edu.pja.prz.receivables.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+public class CashPayment extends BaseEntity<Long> implements Serializable {
+    private LocalDate transactionDate;
+    private String contractorDetails;
+    private String title;
+    private BigDecimal transactionAmount;
+    private String transactionCurrency;
+    private UUID childId;
+    private UUID guardianId;
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getContractorDetails() {
+        return contractorDetails;
+    }
+
+    public void setContractorDetails(String contractorDetails) {
+        this.contractorDetails = contractorDetails;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(BigDecimal transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public String getTransactionCurrency() {
+        return transactionCurrency;
+    }
+
+    public void setTransactionCurrency(String transactionCurrency) {
+        this.transactionCurrency = transactionCurrency;
+    }
+
+    public UUID getChildId() {
+        return childId;
+    }
+
+    public void setChildId(UUID childId) {
+        this.childId = childId;
+    }
+
+    public UUID getGuardianId() {
+        return guardianId;
+    }
+
+    public void setGuardianId(UUID guardianId) {
+        this.guardianId = guardianId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CashPayment that = (CashPayment) o;
+        return transactionDate.equals(that.transactionDate) &&
+                contractorDetails.equals(that.contractorDetails) &&
+                title.equals(that.title) &&
+                transactionAmount.equals(that.transactionAmount) &&
+                transactionCurrency.equals(that.transactionCurrency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionDate, contractorDetails, title, transactionAmount, transactionCurrency);
+    }
+}
