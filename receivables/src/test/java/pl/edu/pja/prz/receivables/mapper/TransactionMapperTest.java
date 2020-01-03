17499e7a700971200f5be8b547b2091e51f20689
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-01-04 00:28:42 +0100

    Fix issues with DTO mapper unit tests

--- receivables/src/test/java/pl/edu/pja/prz/receivables/mapper/TransactionMapperTest.java
+++ receivables/src/test/java/pl/edu/pja/prz/receivables/mapper/TransactionMapperTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import pl.edu.pja.prz.receivables.model.CashPayment;
 import pl.edu.pja.prz.receivables.model.Transaction;
 import pl.edu.pja.prz.receivables.model.dto.IncomingPaymentDto;
 import pl.edu.pja.prz.receivables.model.enums.PaymentType;
@@ -20,10 +19,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class CashPaymentMapperTest {
+public class TransactionMapperTest {
     @Mock
-    private CashPaymentMapper cashPaymentMapper;
-    private CashPayment cashPayment;
+    private TransactionMapper transactionMapper;
+
+    private Transaction transaction;
     private IncomingPaymentDto dto;
 
     private LocalDate date;
@@ -32,19 +32,19 @@ class CashPaymentMapperTest {
 
     @BeforeEach
     public void setUp() {
-        cashPayment = new CashPayment();
+        transaction = new Transaction();
         dto = new IncomingPaymentDto();
         guardianId = UUID.randomUUID();
         childId = UUID.randomUUID();
         date = LocalDate.now();
 
-        cashPayment.setTitle("Czesne #001");
-        cashPayment.setContractorDetails("Adam XYZ");
-        cashPayment.setTransactionAmount(new BigDecimal("50.00"));
-        cashPayment.setTransactionCurrency("PLN");
-        cashPayment.setTransactionDate(date);
-        cashPayment.setChildId(childId);
-        cashPayment.setGuardianId(guardianId);
+        transaction.setTitle("Czesne #001");
+        transaction.setContractorDetails("Adam XYZ");
+        transaction.setTransactionAmount(new BigDecimal("50.00"));
+        transaction.setTransactionCurrency("PLN");
+        transaction.setTransactionDate(date);
+        transaction.setChildId(childId);
+        transaction.setGuardianId(guardianId);
 
         dto.setTitle("Czesne #001");
         dto.setContractorDetails("Adam XYZ");
@@ -56,18 +56,18 @@ class CashPaymentMapperTest {
     }
 
     @Test
-    public void Should_MapCashPayment() {
+    public void Should_MapTransaction() {
         //Given
-        dto.setPaymentType(PaymentType.CASH);
-        when(cashPaymentMapper.cashPaymentToDto(any(CashPayment.class))).thenReturn(dto);
+        dto.setPaymentType(PaymentType.TRANSFER);
+        when(transactionMapper.transactionToDto(any(Transaction.class))).thenReturn(dto);
 
         //When
-        IncomingPaymentDto incomingPaymentDto = cashPaymentMapper.cashPaymentToDto(cashPayment);
+        IncomingPaymentDto incomingPaymentDto = transactionMapper.transactionToDto(transaction);
 
         //Then
         verifyDto(incomingPaymentDto);
-        assertEquals(PaymentType.CASH, incomingPaymentDto.getPaymentType());
-        verify(cashPaymentMapper, times(1)).cashPaymentToDto(any(CashPayment.class));
+        assertEquals(PaymentType.TRANSFER, incomingPaymentDto.getPaymentType());
+        verify(transactionMapper, times(1)).transactionToDto(any(Transaction.class));
     }
 
     private void verifyDto(IncomingPaymentDto dto) {
