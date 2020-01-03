d5ee496d7df386eed6a59953f171d5d42c03adb5
Grzegorz <s12583@pjwstk.edu.pl>
2020-01-04 00:23:23 +0100

    Add PaymentController

--- payments/src/main/java/pl/edu/pja/prz/payments/facade/RecurringPaymentFacade.java
+++ payments/src/main/java/pl/edu/pja/prz/payments/facade/RecurringPaymentFacade.java
@@ -1,68 +1,21 @@
 package pl.edu.pja.prz.payments.facade;
 
-import org.springframework.stereotype.Component;
 import pl.edu.pja.prz.payments.facade.dto.RecurringPaymentDto;
-import pl.edu.pja.prz.payments.facade.mapper.RecurringMapper;
-import pl.edu.pja.prz.payments.service.RecurringPaymentService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Component
-public class RecurringPaymentFacade {
-	private final RecurringPaymentService recurringPaymentService;
-	private final RecurringMapper recurringMapper;
+public interface RecurringPaymentFacade {
+	RecurringPaymentDto createTuition(RecurringPaymentDto recurringPaymentDto);
 
-	public RecurringPaymentFacade(RecurringPaymentService recurringPaymentService, RecurringMapper recurringMapper) {
-		this.recurringPaymentService = recurringPaymentService;
-		this.recurringMapper = recurringMapper;
-	}
+	RecurringPaymentDto createOtherPayment(RecurringPaymentDto recurringPaymentDto);
 
-	public RecurringPaymentDto createTuition(RecurringPaymentDto recurringPaymentDto) {
-		return recurringMapper.fromRecurringPayment(
-				recurringPaymentService.createTuition(
-						recurringMapper.toRecurringPayment(recurringPaymentDto)
-				)
-		);
-	}
+	RecurringPaymentDto findById(Long id);
 
-	public RecurringPaymentDto createOtherPayment(RecurringPaymentDto recurringPaymentDto) {
-		return recurringMapper.fromRecurringPayment(
-				recurringPaymentService.createOtherPayment(
-						recurringMapper.toRecurringPayment(recurringPaymentDto)
-				)
-		);
-	}
+	List<RecurringPaymentDto> findAllPayments();
 
-	public RecurringPaymentDto findById(Long id) {
-		return recurringMapper.fromRecurringPayment(
-						recurringPaymentService.getPaymentById(id)
-				);
-	}
+	RecurringPaymentDto updatePayment(RecurringPaymentDto recurringPayment);
 
-	public List<RecurringPaymentDto> findAllPayments() {
-		return recurringPaymentService.getAllPayments()
-				.stream()
-				.map(recurringMapper::fromRecurringPayment)
-				.collect(Collectors.toList());
-	}
+	RecurringPaymentDto markAsCancelPayment(Long paymentId);
 
-	public RecurringPaymentDto updatePayment(RecurringPaymentDto recurringPayment) {
-		return recurringMapper.fromRecurringPayment(
-				recurringPaymentService.updatePayment(
-						recurringMapper.toRecurringPayment(recurringPayment)
-				)
-		);
-	}
-
-	public RecurringPaymentDto markAsCancelPayment(Long paymentId) {
-		return recurringMapper.fromRecurringPayment(
-				recurringPaymentService.markAsCancelPayment(paymentId)
-		);
-	}
-
-	public void deletePayment(Long id) {
-		recurringPaymentService.deletePayment(id);
-	}
-
+	void deletePayment(Long id);
 }
