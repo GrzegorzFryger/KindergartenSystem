04d4f1d9345c9fdc1b9325c59682d35e50485777
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-01-18 16:12:46 +0100

    Add all required configurations

--- .database/insert_1_0.sql
+++ .database/insert_1_0.sql
@@ -1,4 +1,5 @@
 -- ACCOUNT MODULE --
+-- CALENDAR MODULE --
 -- CORE MODULE --
 -- FINANCES MODULE --
 -- GROUPS MODULE --
@@ -15,8 +16,6 @@ INSERT INTO receivables.transaction (`id`,`version`,`accountNumber`,`bankName`,`
 VALUES (4,0,'56 1090 1014 0000 4467 2354 1423','Bank Millennium','2019-11-12',NULL,'Antoni Michalkiewicz','Za dziecko XYZ',NULL,'432673408632',366.70,'PLN','2019-11-12','201967122332622451');
 INSERT INTO receivables.transaction (`id`,`version`,`accountNumber`,`bankName`,`bookingDate`,`childId`,`contractorDetails`,`details`,`guardianId`,`title`,`transactionAmount`,`transactionCurrency`,`transactionDate`,`transactionNumber`)
 VALUES (5,0,'56 1090 1014 0000 4467 2354 1423','Bank Millennium','2019-12-12',NULL,'Antoni Michalkiewicz','Za dziecko XYZ',NULL,'432673408632',623.70,'PLN','2019-12-12','201963374373423451');
-
-UPDATE receivables.hibernate_sequence SET next_val = 6;
 
 -- FINAL COMMIT FOR ALL CHANGES
 COMMIT;
