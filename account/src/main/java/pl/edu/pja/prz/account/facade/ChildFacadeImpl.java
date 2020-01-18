2667a0e0b6a8a1e8672813d1de26e1b4e23b8848
Adam <s15814@pjwstk.edu.pl>
2020-01-18 14:51:12 +0100

    Change remaining facades to interface + impl

---
+++ account/src/main/java/pl/edu/pja/prz/account/facade/ChildFacadeImpl.java
@@ -0,0 +1,65 @@
+package pl.edu.pja.prz.account.facade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+import pl.edu.pja.prz.account.facade.dto.ChildDto;
+import pl.edu.pja.prz.account.facade.mapper.ChildMapper;
+import pl.edu.pja.prz.commons.model.FullName;
+import pl.edu.pja.prz.account.service.ChildService;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ChildFacadeImpl implements ChildFacade {
+	private final ChildMapper childMapper;
+	private final ChildService childService;
+
+	@Autowired
+	public ChildFacadeImpl(ChildMapper childMapper, ChildService childService) {
+		this.childMapper = childMapper;
+		this.childService = childService;
+	}
+
+	public ChildDto createChild(ChildDto childDto) {
+		if (childDto.getPesel() != null) {
+			return childMapper.fromChild(
+					childService.createChild(
+							childDto.getBoroughId(),
+							childMapper.toAddress(childDto),
+							childMapper.toFullName(childDto),
+							childDto.getPesel(),
+							childMapper.toStudyPeriod(childDto)
+					)
+			);
+
+		} else {
+			return childMapper.fromChild(
+					childService.createChild(
+							childDto.getBoroughId(),
+							childMapper.toAddress(childDto),
+							childMapper.toAge(childDto),
+							childMapper.toFullName(childDto),
+							childDto.getGender(),
+							childMapper.toStudyPeriod(childDto)
+					)
+			);
+		}
+
+	}
+
+	public ChildDto findChildById(UUID id) {
+		return childMapper.fromChild(
+				childService.getChildById(id)
+		);
+	}
+
+	public Optional<ChildDto> findByFullNameOrAddress(String name, String surname, @Nullable String street)
+			throws IllegalStateException {
+		return childService
+				.findByFullNameOrAddressReadOnly(new FullName(name.toLowerCase(), surname.toLowerCase()), street)
+				.map(childMapper::fromChild)
+				.or(Optional::empty);
+	}
+}
---
+++ account/src/main/java/pl/edu/pja/prz/account/facade/EmployeeFacadeImpl.java
@@ -0,0 +1,40 @@
+package pl.edu.pja.prz.account.facade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.edu.pja.prz.account.facade.dto.AccountDto;
+import pl.edu.pja.prz.account.facade.dto.EmployeeDto;
+import pl.edu.pja.prz.account.facade.mapper.AccountMapper;
+import pl.edu.pja.prz.account.facade.mapper.EmployeeMapper;
+import pl.edu.pja.prz.account.service.EmployeeService;
+
+@Service
+public class EmployeeFacadeImpl implements EmployeeFacade {
+    private EmployeeMapper employeeMapper;
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeFacadeImpl(EmployeeMapper employeeMapper, EmployeeService employeeService, AccountMapper accountMapper) {
+        this.employeeMapper = employeeMapper;
+        this.employeeService = employeeService;
+    }
+
+    public EmployeeDto createEmployee(AccountDto accountDto) {
+        return employeeMapper.fromEmployee(
+                employeeService.createEmployeeAccount(
+                        employeeMapper.toPerson(accountDto),
+                        accountDto.getEmail()
+                )
+        );
+    }
+
+    public EmployeeDto createAdministratorAccount(AccountDto accountDto) {
+        return employeeMapper.fromEmployee(
+                employeeService.createAdministratorAccount(
+                        employeeMapper.toPerson(accountDto),
+                        accountDto.getEmail()
+                )
+        );
+    }
+
+}
