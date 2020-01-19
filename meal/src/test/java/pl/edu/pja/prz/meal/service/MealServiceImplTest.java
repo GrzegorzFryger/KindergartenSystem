136a59d541455aa28455d1c875201bd5c05e5daf
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-01-19 10:44:11 +0100

    Add tests to createMeal method

--- meal/src/test/java/pl/edu/pja/prz/meal/service/MealServiceImplTest.java
+++ meal/src/test/java/pl/edu/pja/prz/meal/service/MealServiceImplTest.java
@@ -1,14 +1,28 @@
 package pl.edu.pja.prz.meal.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.edu.pja.prz.meal.exception.MealActivityStatusException;
+import pl.edu.pja.prz.meal.model.Meal;
 import pl.edu.pja.prz.meal.model.dto.MealCreateUpdateDTO;
+import pl.edu.pja.prz.meal.model.enums.MealStatus;
+import pl.edu.pja.prz.meal.model.enums.MealType;
 import pl.edu.pja.prz.meal.repository.MealPriceRepository;
 import pl.edu.pja.prz.meal.repository.MealRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MealServiceImplTest {
@@ -24,15 +38,47 @@ public class MealServiceImplTest {
 
     @BeforeEach
     public void setup() {
-        mealService = new MealServiceImpl(mealRepository, new MealPriceServiceImpl(mealPriceListRepository));
+        MealPriceServiceImpl mealPriceService = new MealPriceServiceImpl(mealPriceListRepository);
+        mealService = new MealServiceImpl(mealRepository, mealPriceService );
+
+        mealCreateUpdateDTO =
+                new MealCreateUpdateDTO(new BigDecimal(88), MealType.BREAKFAST, UUID.randomUUID(), LocalDate.MIN, LocalDate.MAX);
     }
 
 
     @Test
-    public void createMeal() throws MealActivityStatusException {
-        mealService.createMeal(mealCreateUpdateDTO);
+    public void shouldCreateMeal_When_InputArgumentIsCorrect() throws MealActivityStatusException {
+
+        //given
+        Meal meal = new Meal(new BigDecimal(99), LocalDateTime.MIN, LocalDateTime.MAX, MealStatus.ACTIVE, MealType.BREAKFAST, UUID.randomUUID());
+
+        //when
+        when(mealRepository.findMealByChildIDAndMealStatusAndMealType(any(), any(), any())).thenReturn(Optional.empty());
+        when(mealRepository.save(any())).thenReturn(meal);
+        Meal createdMeal = mealService.createMeal(mealCreateUpdateDTO);
+
+        //then
+        Assert.assertNotNull(createdMeal);
+        Assert.assertEquals(createdMeal.getMealStatus(), MealStatus.ACTIVE);
+        Assert.assertNotNull(createdMeal.getMealPrice());
+        Assert.assertNotNull(createdMeal.getChildID());
     }
 
+
+    @Test
+    public void shouldThrowMealActivityStatusException_When_TriedToCreateMealWhichIsAlreadyActive() {
+
+        //given
+        Meal meal = new Meal(new BigDecimal(99), LocalDateTime.MIN, LocalDateTime.MAX, MealStatus.ACTIVE, MealType.BREAKFAST, UUID.randomUUID());
+
+        //when
+        when(mealRepository.findMealByChildIDAndMealStatusAndMealType(any(), any(), any())).thenReturn(Optional.of(meal));
+
+        //then
+        assertThrows(MealActivityStatusException.class, () -> {
+            mealService.createMeal(mealCreateUpdateDTO);
+        });
+    }
     @Test
     public void getMealByID() {
     }
