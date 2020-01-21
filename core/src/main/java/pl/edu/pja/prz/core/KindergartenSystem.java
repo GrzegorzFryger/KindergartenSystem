7f559152091422a9251b24174f5f6192e1434490
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-01-21 18:31:47 +0100

    Fix travis build

--- core/src/main/java/pl/edu/pja/prz/core/KindergartenSystem.java
+++ core/src/main/java/pl/edu/pja/prz/core/KindergartenSystem.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import pl.edu.pja.prz.account.AccountConfiguration;
-import pl.edu.pja.prz.calendar.CalendarConfiguration;
 import pl.edu.pja.prz.commons.CommonsConfiguration;
 import pl.edu.pja.prz.finances.FinancesConfiguration;
 import pl.edu.pja.prz.groups.GroupsConfiguration;
@@ -16,7 +15,7 @@ import pl.edu.pja.prz.receivables.ReceivablesConfiguration;
 @Import({AccountConfiguration.class, GroupsConfiguration.class,
 		ReceivablesConfiguration.class, PaymentsConfiguration.class,
 		FinancesConfiguration.class, CommonsConfiguration.class,
-		CalendarConfiguration.class, MealConfiguration.class})
+		MealConfiguration.class})
 public class KindergartenSystem {
 	public static void main(String[] args) {
 		SpringApplication.run(KindergartenSystem.class, args);
