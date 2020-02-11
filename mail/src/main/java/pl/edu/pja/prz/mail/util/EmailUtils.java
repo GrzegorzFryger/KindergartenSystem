9b8fb243ab8b2fc6e35103101de8758e9fd465e4
Grzegorz <s12583@pjwstk.edu.pl>
2020-02-11 14:39:11 +0100

    Restore email validator

---
+++ mail/src/main/java/pl/edu/pja/prz/mail/util/EmailUtils.java
@@ -0,0 +1,11 @@
+package pl.edu.pja.prz.mail.util;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class EmailUtils {
+    private EmailUtils() {
+    }
+    public static boolean validateEmail(String email) {
+        return EmailValidator.getInstance().isValid(email);
+    }
+}
