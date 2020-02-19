d0902416420bfc36909dd217a1c61dadea3dbee1
Adam <s15814@pjwstk.edu.pl>
2020-02-19 20:36:55 +0100

    Add incomplete service for absences

---
+++ calendar/src/main/java/pl/edu/pja/prz/calendar/service/AbsenceServiceImpl.java
@@ -0,0 +1,39 @@
+package pl.edu.pja.prz.calendar.service;
+
+import pl.edu.pja.prz.calendar.model.Absence;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public class AbsenceServiceImpl implements AbsenceService {
+	@Override
+	public Absence createAbsence(Absence Absence) {
+		return null;
+	}
+
+	@Override
+	public Absence updateAbsence(Absence Absence) {
+		return null;
+	}
+
+	@Override
+	public void deleteAbsence(Long id) {
+
+	}
+
+	@Override
+	public Absence getAbsence(Long id) {
+		return null;
+	}
+
+	@Override
+	public List<Absence> getAllAbsencesByDate(LocalDate date) {
+		return null;
+	}
+
+	@Override
+	public List<Absence> getAllAbsencesByChildId(UUID id) {
+		return null;
+	}
+}
