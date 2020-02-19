d48e8d64333c631c7a022a0dd29db69e1f4d1548
Adam <s15814@pjwstk.edu.pl>
2020-02-19 20:25:23 +0100

    Add incomplete service for days off work

---
+++ calendar/src/main/java/pl/edu/pja/prz/calendar/service/DayOffWorkServiceImpl.java
@@ -0,0 +1,32 @@
+package pl.edu.pja.prz.calendar.service;
+
+import pl.edu.pja.prz.calendar.model.DayOffWork;
+
+import java.util.List;
+
+public class DayOffWorkServiceImpl implements DayOffWorkService {
+	@Override
+	public DayOffWork createDayOffWork(DayOffWork dayOffWork) {
+		return null;
+	}
+
+	@Override
+	public DayOffWork updateDayOffWork(DayOffWork dayOffWork) {
+		return null;
+	}
+
+	@Override
+	public void deleteDayOffWork(Long id) {
+
+	}
+
+	@Override
+	public DayOffWork getDayOffWork(Long id) {
+		return null;
+	}
+
+	@Override
+	public List<DayOffWork> getAllDaysOff() {
+		return null;
+	}
+}
