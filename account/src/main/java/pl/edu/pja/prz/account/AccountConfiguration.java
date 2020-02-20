0268c087ff798686b538fcc03f1b4c40d16503e5
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-02-20 20:25:26 +0100

    Proof of concept - JWT token based authorization
    Mechanism is based on followning articles:
    https://jakublesko.com/spring-security-with-jwt/
    
    And on following library:
    https://github.com/jwtk/jjwt

--- account/src/main/java/pl/edu/pja/prz/account/AccountConfiguration.java
+++ account/src/main/java/pl/edu/pja/prz/account/AccountConfiguration.java
@@ -2,12 +2,9 @@ package pl.edu.pja.prz.account;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
 
@@ -16,16 +13,11 @@ import javax.annotation.PostConstruct;
 @ComponentScan
 @PropertySource("classpath:application-account.properties")
 public class AccountConfiguration {
-	private static final Logger logger = LoggerFactory.getLogger(AccountConfiguration.class);
+    private static final Logger logger = LoggerFactory.getLogger(AccountConfiguration.class);
 
-	@PostConstruct
-	public void postConstruct() {
-		logger.info("Load ACCOUNT MODULE");
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("Load ACCOUNT MODULE");
+    }
 
 }
--- core/src/main/java/pl/edu/pja/prz/core/configuration/SecurityConfiguration.java
+++ core/src/main/java/pl/edu/pja/prz/core/configuration/SecurityConfiguration.java
@@ -1,19 +1,60 @@
 package pl.edu.pja.prz.core.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import pl.edu.pja.prz.core.filter.JwtAuthenticationFilter;
+import pl.edu.pja.prz.core.filter.JwtAuthorizationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static pl.edu.pja.prz.core.configuration.SecurityConstants.AUTH_LOGIN_URL;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers(AUTH_LOGIN_URL).permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .requiresChannel().anyRequest().requiresSecure()
                 .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
                 .csrf().disable();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .authorities("ROLE_USER");
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+
+        return source;
     }
 }
