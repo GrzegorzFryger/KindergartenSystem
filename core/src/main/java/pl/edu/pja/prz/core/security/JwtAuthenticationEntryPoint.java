010fb1815f83b2e89f885083476beb6e79906bf5
Grzegorz <s12583@pjwstk.edu.pl>
2020-02-25 00:52:33 +0100

    Add temporary UNAUTHORIZED error handling

---
+++ core/src/main/java/pl/edu/pja/prz/core/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package pl.edu.pja.prz.core.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
+	public JwtAuthenticationEntryPoint(String loginUrl)  {
+		super(loginUrl);
+	}
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication Failed");
+	}
+}
