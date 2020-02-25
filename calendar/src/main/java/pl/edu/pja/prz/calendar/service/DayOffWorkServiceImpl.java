c9352e6026d0c9541a10309fc3e4f4810cb272a7
Adam <s15814@pjwstk.edu.pl>
2020-02-25 17:44:03 +0100

    Implement methods in DayOffWork service

--- calendar/src/main/java/pl/edu/pja/prz/calendar/service/DayOffWorkServiceImpl.java
+++ calendar/src/main/java/pl/edu/pja/prz/calendar/service/DayOffWorkServiceImpl.java
@@ -1,32 +1,65 @@
 package pl.edu.pja.prz.calendar.service;
 
 import pl.edu.pja.prz.calendar.model.DayOffWork;
+import pl.edu.pja.prz.calendar.repository.DayOffWorkRepository;
+import pl.edu.pja.prz.commons.exception.ElementNotFoundException;
 
 import java.util.List;
 
 public class DayOffWorkServiceImpl implements DayOffWorkService {
+	private static final String DAYOFF = "Day off";
+
+	private final DayOffWorkRepository dayOffWorkRepository;
+
+	public DayOffWorkServiceImpl(DayOffWorkRepository dayOffWorkRepository) {
+		this.dayOffWorkRepository = dayOffWorkRepository;
+	}
+
 	@Override
 	public DayOffWork createDayOffWork(DayOffWork dayOffWork) {
-		return null;
+		return dayOffWorkRepository.save(dayOffWork);
 	}
 
 	@Override
 	public DayOffWork updateDayOffWork(DayOffWork dayOffWork) {
-		return null;
+		if (dayOffWorkRepository.findById(dayOffWork.getId()).isEmpty()) {
+			throw new ElementNotFoundException(DAYOFF, dayOffWork.getId());
+		}
+
+		DayOffWork dayOffToUpdate = getDayOffWork(dayOffWork.getId());
+
+		if (dayOffToUpdate.getDate() != null) {
+			dayOffToUpdate.setDate(dayOffWork.getDate());
+		}
+
+		if (dayOffToUpdate.getName() != null) {
+			dayOffToUpdate.setName(dayOffWork.getName());
+		}
+
+		if (dayOffToUpdate.getEventType() != null) {
+			dayOffToUpdate.setEventType(dayOffWork.getEventType());
+		}
+
+		return dayOffWorkRepository.save(dayOffToUpdate);
 	}
 
 	@Override
 	public void deleteDayOffWork(Long id) {
+		if (dayOffWorkRepository.findById(id).isEmpty()) {
+			throw new ElementNotFoundException(DAYOFF, id);
+		}
+		dayOffWorkRepository.deleteById(id);
 
 	}
 
 	@Override
 	public DayOffWork getDayOffWork(Long id) {
-		return null;
+		return dayOffWorkRepository.findById(id).orElseThrow(
+				() -> new ElementNotFoundException(DAYOFF, id));
 	}
 
 	@Override
 	public List<DayOffWork> getAllDaysOff() {
-		return null;
+		return dayOffWorkRepository.findAll();
 	}
 }
