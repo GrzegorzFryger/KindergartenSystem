cce111df36b24d33000d214f987a371953bd12bc
Adam <s15814@pjwstk.edu.pl>
2020-03-04 21:45:38 +0100

    Add @PreAuthorize annotations to controllers

--- core/src/main/java/pl/edu/pja/prz/core/controller/calendar/AbsenceController.java
+++ core/src/main/java/pl/edu/pja/prz/core/controller/calendar/AbsenceController.java
@@ -3,6 +3,7 @@ package pl.edu.pja.prz.core.controller.calendar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pja.prz.calendar.facade.AbsenceFacade;
 import pl.edu.pja.prz.calendar.model.dto.AbsenceDto;
@@ -21,38 +22,45 @@ public class AbsenceController {
 		this.absenceFacade = absenceFacade;
 	}
 
+	@PreAuthorize("hasRole('USER') or hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@GetMapping("absence/{id}")
 	public ResponseEntity<AbsenceDto> findAbsence(@PathVariable Long id) {
 		return new ResponseEntity<>(absenceFacade.getAbsence(id), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@PostMapping("absence")
 	public ResponseEntity<AbsenceDto> createAbsence(@RequestBody AbsenceDto absenceDto) {
 		return new ResponseEntity<>(absenceFacade.createAbsence(absenceDto), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@PutMapping("absence")
 	public ResponseEntity<AbsenceDto> updateAbsence(@RequestBody AbsenceDto absenceDto) {
 		return new ResponseEntity<>(absenceFacade.updateAbsence(absenceDto), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@DeleteMapping("absence/{id}")
 	public ResponseEntity<?> deleteAbsence(@PathVariable Long id) {
 		absenceFacade.deleteAbsence(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('USER') or hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@GetMapping("absence/childById/{childId}")
 	public ResponseEntity<List<AbsenceDto>> getAllAbsencesByChildId(@PathVariable UUID childId) {
 		return new ResponseEntity<>(absenceFacade.getAllAbsencesByChildId(childId), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@GetMapping("absence/childByDate/{date}")
 	public ResponseEntity<List<AbsenceDto>> getAllAbsencesByDate(@PathVariable String date) {
 		LocalDate dateToCheck = LocalDate.parse(date);
 		return new ResponseEntity<>(absenceFacade.getAllAbsencesByDate(dateToCheck), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('USER') or hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@GetMapping("absence/child/{childId}/{startDate}/{endDate}")
 	public ResponseEntity<List<AbsenceDto>> getAllAbsencesForChildBetweenDates(@PathVariable UUID childId,
 																			   @PathVariable String startDate,
--- core/src/main/java/pl/edu/pja/prz/core/controller/calendar/DayOffWorkController.java
+++ core/src/main/java/pl/edu/pja/prz/core/controller/calendar/DayOffWorkController.java
@@ -3,6 +3,7 @@ package pl.edu.pja.prz.core.controller.calendar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pja.prz.calendar.facade.DayOffWorkFacade;
 import pl.edu.pja.prz.calendar.model.dto.DayOffWorkDto;
@@ -19,26 +20,31 @@ public class DayOffWorkController {
 		this.dayOffWorkFacade = dayOffWorkFacade;
 	}
 
+	@PreAuthorize("hasRole('USER') or hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@GetMapping("dayoff/{id}")
 	public ResponseEntity<DayOffWorkDto> findDayOffWork(@PathVariable Long id) {
 		return new ResponseEntity<>(dayOffWorkFacade.getDayOffWork(id), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('USER') or hasRole('TEACHER') or hasRole('ADMINISTRATOR')")
 	@GetMapping("daysoff")
 	public ResponseEntity<List<DayOffWorkDto>> findAllDaysOffWork() {
 		return new ResponseEntity<>(dayOffWorkFacade.getAllDaysOff(), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('ADMINISTRATOR')")
 	@PostMapping("dayoff")
 	public ResponseEntity<DayOffWorkDto> createDayOffWork(@RequestBody DayOffWorkDto dayOffWorkDto) {
 		return new ResponseEntity<>(dayOffWorkFacade.createDayOffWork(dayOffWorkDto), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('ADMINISTRATOR')")
 	@PutMapping("dayoff")
 	public ResponseEntity<DayOffWorkDto> updateDayOffWork(@RequestBody DayOffWorkDto dayOffWorkDto) {
 		return new ResponseEntity<>(dayOffWorkFacade.updateDayOffWork(dayOffWorkDto), HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('ADMINISTRATOR')")
 	@DeleteMapping("dayoff/{id}")
 	public ResponseEntity<?> deleteDayOffWork(@PathVariable Long id) {
 		dayOffWorkFacade.deleteDayOffWork(id);
