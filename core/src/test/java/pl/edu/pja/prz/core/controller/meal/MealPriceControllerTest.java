9ef604efb912499d7812e3bc83bff33b11147230
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-16 18:43:18 +0100

    Add tests to meal price controller

--- core/src/test/java/pl/edu/pja/prz/core/controller/meal/MealPriceControllerTest.java
+++ core/src/test/java/pl/edu/pja/prz/core/controller/meal/MealPriceControllerTest.java
@@ -1,21 +1,31 @@
 package pl.edu.pja.prz.core.controller.meal;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.edu.pja.prz.meal.facade.MealPriceFacade;
 import pl.edu.pja.prz.meal.model.MealPrice;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class MealPriceControllerTest {
 
-    @Mock
+
     private MealPriceController mealPriceController;
     private MealPrice mealPrice;
+
+    @Mock
+    private MealPriceFacade mealPriceFacade;
+
+    @BeforeEach
+    public void setUp() {
+        mealPriceController = new MealPriceController(mealPriceFacade);
+    }
+
 
     @Test
     void creatMealPrice() {
@@ -23,7 +33,7 @@ class MealPriceControllerTest {
         mealPriceController.creatMealPrice(mealPrice);
 
         //then
-        verify(mealPriceController, times(1)).creatMealPrice(mealPrice);
+        verify(mealPriceFacade, times(1)).creatMealPrice(mealPrice);
     }
 
     @Test
@@ -32,7 +42,7 @@ class MealPriceControllerTest {
         mealPriceController.updateMealPrice(mealPrice, 1L);
 
         //then
-        verify(mealPriceController, times(1)).updateMealPrice(mealPrice, 1L);
+        verify(mealPriceFacade, times(1)).updateMealPrice(mealPrice, 1L);
     }
 
     @Test
@@ -41,7 +51,7 @@ class MealPriceControllerTest {
         mealPriceController.getAllPrices();
 
         //then
-        verify(mealPriceController, times(1)).getAllPrices();
+        verify(mealPriceFacade, times(1)).getAllPrices();
     }
 
     @Test
@@ -50,6 +60,6 @@ class MealPriceControllerTest {
         mealPriceController.deleteMealPriceById(1);
 
         //then
-        verify(mealPriceController, times(1)).deleteMealPriceById(1);
+        verify(mealPriceFacade, times(1)).deleteMealPriceById(1);
     }
 }
