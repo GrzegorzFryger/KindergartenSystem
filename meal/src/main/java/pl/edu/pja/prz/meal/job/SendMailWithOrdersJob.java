b9447dc08642732208e60c38df67bf23f96897e2
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-18 18:34:35 +0100

    Add Meal cron Job

---
+++ meal/src/main/java/pl/edu/pja/prz/meal/job/SendMailWithOrdersJob.java
@@ -0,0 +1,19 @@
+package pl.edu.pja.prz.meal.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import pl.edu.pja.prz.meal.service.MealServiceImpl;
+import pl.edu.pja.prz.scheduler.annotation.QuartzJob;
+
+@QuartzJob(name = "meals", description = "send mail with meals order")
+public class SendMailWithOrdersJob extends MealJob{
+
+    public SendMailWithOrdersJob(MealServiceImpl mealService) {
+        super(mealService);
+    }
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        mealService.sendEmailWithOrder();
+    }
+}
