f198b992fb3f15350c9b0b20f4217533f9f97236
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-27 22:02:21 +0100

    Create NutritionalNotes service

---
+++ meal/src/main/java/pl/edu/pja/prz/meal/service/NutritionalNotesService.java
@@ -0,0 +1,7 @@
+package pl.edu.pja.prz.meal.service;
+
+import pl.edu.pja.prz.meal.model.NutritionalNotes;
+
+public interface NutritionalNotesService {
+    NutritionalNotes addNutritionalNotes()
+}
