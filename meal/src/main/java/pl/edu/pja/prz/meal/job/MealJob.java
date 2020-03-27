1b00a535d1e013152995ceb93be0781c31753053
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-27 18:01:42 +0100

    Integration with calendar module

--- meal/src/main/java/pl/edu/pja/prz/meal/job/MealJob.java
+++ meal/src/main/java/pl/edu/pja/prz/meal/job/MealJob.java
@@ -3,15 +3,18 @@ package pl.edu.pja.prz.meal.job;
 import org.quartz.Job;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pl.edu.pja.prz.calendar.service.DayOffWorkService;
 import pl.edu.pja.prz.meal.service.MealServiceImpl;
 
 @Component
 public abstract class MealJob implements Job {
 
     protected MealServiceImpl mealService;
+    protected DayOffWorkService dayOffWorkService;
 
     @Autowired
-    public MealJob(MealServiceImpl mealService) {
+    public MealJob(MealServiceImpl mealService, DayOffWorkService dayOffWorkService) {
+        this.dayOffWorkService = dayOffWorkService;
         this.mealService = mealService;
     }
 }
