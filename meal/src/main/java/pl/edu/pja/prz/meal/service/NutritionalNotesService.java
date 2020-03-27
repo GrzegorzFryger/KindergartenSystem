4433c99ac763341040094ad61cda0ba02b6902a7
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-27 22:05:02 +0100

    Create NutritionalNotes service

--- meal/src/main/java/pl/edu/pja/prz/meal/service/NutritionalNotesService.java
+++ meal/src/main/java/pl/edu/pja/prz/meal/service/NutritionalNotesService.java
@@ -1,7 +1,15 @@
 package pl.edu.pja.prz.meal.service;
 
 import pl.edu.pja.prz.meal.model.NutritionalNotes;
+import pl.edu.pja.prz.meal.model.dto.NutritionalNotesDTO;
+
+import java.util.List;
 
 public interface NutritionalNotesService {
-    NutritionalNotes addNutritionalNotes()
+
+    List<NutritionalNotes> getAllNutritionalNotes();
+    NutritionalNotes addNutritionalNotes(NutritionalNotesDTO nutritionalNotesDTO);
+    NutritionalNotes getNutritionalNotesById(Long id);
+    void deleteNutritionalNotesById(Long id);
+
 }
