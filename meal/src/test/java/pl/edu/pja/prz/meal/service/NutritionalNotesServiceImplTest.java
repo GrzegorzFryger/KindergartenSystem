7d3b810277a4e0ba8cdfda280ac272c3fa93858e
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-29 08:35:24 +0200

    Add test to getAllNutritionalNotes method

---
+++ meal/src/test/java/pl/edu/pja/prz/meal/service/NutritionalNotesServiceImplTest.java
@@ -0,0 +1,60 @@
+package pl.edu.pja.prz.meal.service;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.edu.pja.prz.meal.model.NutritionalNotes;
+import pl.edu.pja.prz.meal.repository.MealNutritionalNotesRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class NutritionalNotesServiceImplTest {
+
+    @Mock
+    private MealNutritionalNotesRepository mealNutritionalNotesRepository;
+    @Mock
+    private MealService mealService;
+
+    private NutritionalNotesService nutritionalNotesService;
+    List<NutritionalNotes> nutritionalNotes = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        nutritionalNotesService = new NutritionalNotesServiceImpl(mealNutritionalNotesRepository, mealService);
+    }
+
+
+
+    @Test
+    void getAllNutritionalNotes() {
+        //given
+        nutritionalNotes.add(new NutritionalNotes());
+        nutritionalNotes.add(new NutritionalNotes());
+        nutritionalNotes.add(new NutritionalNotes());
+
+        //when
+        when(mealNutritionalNotesRepository.findAll()).thenReturn(nutritionalNotes);
+
+        //then
+        Assert.assertEquals(nutritionalNotesService.getAllNutritionalNotes(), nutritionalNotes);
+    }
+
+    @Test
+    void addNutritionalNotes() {
+    }
+
+    @Test
+    void getNutritionalNotesById() {
+    }
+
+    @Test
+    void deleteNutritionalNotesById() {
+    }
+}
