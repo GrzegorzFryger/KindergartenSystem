91378028b663b38477668e5e9fd8ddcc2ccc5b3e
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-29 10:09:07 +0200

    Implement MealNutritionalNotesController

--- core/src/main/java/pl/edu/pja/prz/core/controller/meal/MealNutritionalNotesController.java
+++ core/src/main/java/pl/edu/pja/prz/core/controller/meal/MealNutritionalNotesController.java
@@ -1,9 +1,14 @@
 package pl.edu.pja.prz.core.controller.meal;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.pja.prz.meal.facade.MealNutritionalNotesFacade;
+import pl.edu.pja.prz.meal.model.NutritionalNotes;
+import pl.edu.pja.prz.meal.model.dto.NutritionalNotesDTO;
 
+import java.util.List;
+
+import static pl.edu.pja.prz.commons.constants.Roles.HAS_ANY_ROLE;
 import static pl.edu.pja.prz.core.controller.RequestMappings.API_NUTRITIONAL_NOTES;
 
 @RestController
@@ -14,5 +19,29 @@ public class MealNutritionalNotesController {
 
     public MealNutritionalNotesController(MealNutritionalNotesFacade mealNutritionalNotesFacade) {
         this.mealNutritionalNotesFacade = mealNutritionalNotesFacade;
+    }
+
+    @GetMapping
+    @PreAuthorize(HAS_ANY_ROLE)
+    public List<NutritionalNotes> getAllNutritionalNotes() {
+        return mealNutritionalNotesFacade.getAllNutritionalNotes();
+    }
+
+    @PostMapping
+    @PreAuthorize(HAS_ANY_ROLE)
+    NutritionalNotes addNutritionalNotes(@RequestBody NutritionalNotesDTO nutritionalNotesDTO) {
+        return mealNutritionalNotesFacade.addNutritionalNotes(nutritionalNotesDTO);
+    }
+
+    @GetMapping("{id}")
+    @PreAuthorize(HAS_ANY_ROLE)
+    public NutritionalNotes getNutritionalNotesById(@PathVariable("id") Long id) {
+        return mealNutritionalNotesFacade.getNutritionalNotesById(id);
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize(HAS_ANY_ROLE)
+    public void deleteNutritionalNotesById(@PathVariable("id") Long id) {
+        mealNutritionalNotesFacade.deleteNutritionalNotesById(id);
     }
 }
