84ef13d18aef49f1871d4da441c69cac63cc1428
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-29 09:47:44 +0200

    Implement MealNutritionalNotesFacade

--- meal/src/main/java/pl/edu/pja/prz/meal/facade/MealNutritionalNotesFacade.java
+++ meal/src/main/java/pl/edu/pja/prz/meal/facade/MealNutritionalNotesFacade.java
@@ -2,7 +2,11 @@ package pl.edu.pja.prz.meal.facade;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edu.pja.prz.meal.model.NutritionalNotes;
+import pl.edu.pja.prz.meal.model.dto.NutritionalNotesDTO;
 import pl.edu.pja.prz.meal.service.MealNutritionalNotesService;
+
+import java.util.List;
 
 @Service
 public class MealNutritionalNotesFacade {
@@ -12,5 +16,21 @@ public class MealNutritionalNotesFacade {
     @Autowired
     public MealNutritionalNotesFacade(MealNutritionalNotesService mealNutritionalNotesService) {
         this.mealNutritionalNotesService = mealNutritionalNotesService;
+    }
+
+    public List<NutritionalNotes> getAllNutritionalNotes() {
+        return mealNutritionalNotesService.getAllNutritionalNotes();
+    }
+
+    public NutritionalNotes addNutritionalNotes(NutritionalNotesDTO nutritionalNotesDTO) {
+        return mealNutritionalNotesService.addNutritionalNotes(nutritionalNotesDTO);
+    }
+
+    public NutritionalNotes getNutritionalNotesById(Long id) {
+        return mealNutritionalNotesService.getNutritionalNotesById(id);
+    }
+
+    public void deleteNutritionalNotesById(Long id) {
+        mealNutritionalNotesService.deleteNutritionalNotesById(id);
     }
 }
