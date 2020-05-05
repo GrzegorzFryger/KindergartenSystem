2e6500bc2b79bd984991c21174d4b6b40e038520
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-05-05 13:20:14 +0200

    Increase code coverage in core module

---
+++ core/src/test/java/pl/edu/pja/prz/core/controller/payments/PaymentsControllerTest.java
@@ -0,0 +1,138 @@
+package pl.edu.pja.prz.core.controller.payments;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import pl.edu.pja.prz.payments.facade.RecurringPaymentFacade;
+import pl.edu.pja.prz.payments.facade.dto.RecurringPaymentDto;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pl.edu.pja.prz.core.controller.RequestMappings.API_PAYMENTS;
+import static pl.edu.pja.prz.core.util.JsonUtil.convertToJson;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentsControllerTest {
+
+    private MockMvc mvc;
+
+    @Mock
+    private RecurringPaymentFacade recurringPaymentFacade;
+
+    @InjectMocks
+    private PaymentsController controller;
+
+    @BeforeEach
+    public void setUp() {
+        this.mvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void Should_DelegateApiCallTo_findAllPaymentsMethod() throws Exception {
+        //Given
+
+        //When
+        mvc.perform(MockMvcRequestBuilders.get(API_PAYMENTS)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        //Then
+        verify(recurringPaymentFacade, only()).findAllPayments();
+    }
+
+    @Test
+    public void Should_DelegateApiCallTo_findPaymentByIdMethod() throws Exception {
+        //Given
+
+        //When
+        mvc.perform(MockMvcRequestBuilders.get(API_PAYMENTS + "/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        //Then
+        verify(recurringPaymentFacade, only()).findById(anyLong());
+    }
+
+    @Test
+    public void Should_DelegateApiCallTo_createTuitionMethod() throws Exception {
+        //Given
+        String json = convertToJson(new RecurringPaymentDto());
+
+        //When
+        mvc.perform(MockMvcRequestBuilders.post(API_PAYMENTS + "tuition")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk());
+
+        //Then
+        verify(recurringPaymentFacade, only()).createTuition(any(RecurringPaymentDto.class));
+    }
+
+    @Test
+    public void Should_DelegateApiCallTo_createOtherMethod() throws Exception {
+        //Given
+        String json = convertToJson(new RecurringPaymentDto());
+
+        //When
+        mvc.perform(MockMvcRequestBuilders.post(API_PAYMENTS + "other")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk());
+
+        //Then
+        verify(recurringPaymentFacade, only()).createOtherPayment(any(RecurringPaymentDto.class));
+    }
+
+    @Test
+    public void Should_DelegateApiCallTo_updatePaymentMethod() throws Exception {
+        //Given
+        String json = convertToJson(new RecurringPaymentDto());
+
+        //When
+        mvc.perform(MockMvcRequestBuilders.put(API_PAYMENTS)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk());
+
+        //Then
+        verify(recurringPaymentFacade, only()).updatePayment(any(RecurringPaymentDto.class));
+    }
+
+    @Test
+    public void Should_DelegateApiCallTo_deletePaymentByIdMethod() throws Exception {
+        //Given
+
+        //When
+        mvc.perform(MockMvcRequestBuilders.delete(API_PAYMENTS + "/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        //Then
+        verify(recurringPaymentFacade, only()).deletePayment(anyLong());
+    }
+
+    @Test
+    public void Should_DelegateApiCallTo_markAsCancelPaymentMethod() throws Exception {
+        //Given
+        String json = convertToJson(1);
+
+        //When
+        mvc.perform(MockMvcRequestBuilders.put(API_PAYMENTS + "cancel")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk());
+
+        //Then
+        verify(recurringPaymentFacade, only()).markAsCancelPayment(anyLong());
+    }
+}
